6434baaa38dadcefdb8f1522a419718c68b27ebe
Punyawat Thanathatthada <kungpun@hotmail.com>
2020-09-03 08:04:06 +0700

    แก้ของสัปดาห์ที่ 3 ชิ้นที่ 1 ครับ

--- week3work1/work.cpp
+++ week3work1/work.cpp
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
+
 int main() {
 
-	float v{}, s{}, t{};
-	char add{};
+	float v, s, t;
+	char add;
+
 	printf("\nWould you want to find \n\n"); printf("v , s , t");
 	printf("\nEnter : ");
 	scanf_s("%c", &add);
+
 
 	if (add == 'v' || add == 's' || add == 't') {
 
@@ -17,6 +20,7 @@ int main() {
 			scanf_s("%f", &s); if (s < 0) { printf("ERROR"); return 0; }
 			printf("add number T : ");
 			scanf_s("%f", &t); if (t < 0) { printf("ERROR"); return 0; }
+			if (s == 0 || t == 0) { printf("\nV is 0.00 ");  return 0; }
 			printf("\nV is %.2f", s / t); break;
 
 
@@ -26,6 +30,7 @@ int main() {
 			scanf_s("%f", &v); if (v < 0) { printf("ERROR"); return 0; }
 			printf("add number T : ");
 			scanf_s("%f", &t); if (t < 0) { printf("ERROR"); return 0; }
+			if (v == 0 || t == 0) { printf("\nS is 0.00 ");  return 0; }
 			printf("\nS is %.2f", v * t); break;
 
 
@@ -34,6 +39,7 @@ int main() {
 			scanf_s("%f", &v); if (v < 0) { printf("ERROR"); return 0; }
 			printf("add number S : ");
 			scanf_s("%f", &s); if (s < 0) { printf("ERROR"); return 0; }
+			if (s == 0 || v == 0) { printf("\nT is 0.00 ");  return 0; }
 			printf("\nT is %.2f", s / v); break;
 
 			return 0;
@@ -41,11 +47,9 @@ int main() {
 		}
 
 	}
-	else if (add != s || add != v || add != t) printf("ERROR");
+	else if (add != 's' || add != 'v' || add != 't') printf("ERROR");
+
 
 	return 0;
-}
-
-
-
+   }
 
